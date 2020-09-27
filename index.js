9842e99dec9664efaf402ffcfca6bb0d5e097687
Eduardo Bittencourt <biteca@gmail.com>
2020-09-27 17:11:56 -0300

    fausto bot ao ataque

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const { Client } = require('discord.js')
+const app = express()
+
+const client = new Client()
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.login('NzU5ODU3MTk3OTc0NDIxNTI1.X3DmKQ.90wUoBf-ASpsfG_tLt7nVJEYras')
+
+client.on('message', async (message) => {
+  if (!message.content.startsWith('!') || message.author.bot) return
+
+  const [command, ...args] = message.content.slice(1).trim().split(' ')
+
+  switch (command.toLowerCase()) {
+    case 'horacerta':
+      message.channel.send(`@everyone SETE E TRINTA E SETE BITCHO!`)
+      break
+    case 'mute': {
+      const guild = message.guild
+      const voiceChannel = guild.channels.cache.find(
+        (channel) => channel.id === '759850223211249674'
+      )
+
+      voiceChannel.members.forEach((member) => {
+        const voiceState = member.voice
+        voiceState.setMute(true)
+      })
+      break
+    }
+    case 'unmute': {
+      const guild = message.guild
+      const voiceChannel = guild.channels.cache.find(
+        (channel) => channel.id === '759850223211249674'
+      )
+
+      voiceChannel.members.forEach((member) => {
+        const voiceState = member.voice
+        voiceState.setMute(false)
+      })
+      break
+    }
+
+    default:
+      break
+  }
+})
+
+app.listen(3333, () => {
+  console.log('Server started')
+})
